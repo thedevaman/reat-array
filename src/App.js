d2cd0cdc0a191c7fa0a1a0bf010d89c3aedece91
thedevaman <devaman2508@gmail.com>
2022-09-03 09:36:00 +0530

    map function

--- src/App.js
+++ src/App.js
@@ -2,22 +2,48 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ //const students = ['Anil','aman','rahul','akash'];
+  //map used for looping
+  // students.map((item)=>{
+  //   console.warn(item)
+  // });
+  // for(let i=0; i<students.length; i++)
+  // {
+  //   console.warn(students[i])
+  // }
+
+  const students = [
+    {name:'aman',email:'aman@gmail',address:'varanasi'},
+    {name:'rahul',email:'rahul@gmail',address:'varanasi'},
+    {name:'akash',email:'akash@gmail',address:'varanasi'},
+]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Handel With Array</h1>
+      <table border="1">
+      <tr>
+<th>Name</th>
+<th>email</th>
+<th>address</th>
+</tr>
+      {
+      students.map((data)=>
+     
+       <tr>
+        <td>{data.name}</td>
+        <td>{data.email}</td>
+        <td>{data.address}</td>
+        </tr>
+  )
+}
+</table>
+{/* {//in  return only map function is used for loop
+  for(let i=0; i<students.length; i++)
+  {
+    <h1>{students[i]}</h1>
+  }
+} */}
     </div>
   );
 }
